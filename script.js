aab9041fae90d9b97180e6974e81b79591fbad0e
Ernest Bracy <ernestbracy1@gmail.com>
2023-06-21 12:37:25 -0400

    Added a click and edit feature

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log(addBtn);
 
 addBtn.addEventListener("click", () => addNewNote("Add New Note"));
 
-addNewNote = () => {
+addNewNote = (text = "") => {
   const note = document.createElement("div");
   note.classList.add("note");
 
@@ -18,13 +18,25 @@ addNewNote = () => {
   </button>
 </div>
 
-<div class="hidden"></div>
-<textarea></textarea>
+<div class="main ${text ? "" : "hidden"}"></div>
+<textarea class="${text ? "hidden" : ""}"></textarea>
   `;
+
   const deleteBtn = note.querySelector(".delete");
+  const editBtn = note.querySelector(".edit");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+
+  textArea.value = text;
+  main.innerHtml = text;
   //delete function
   deleteBtn.addEventListener("click", () => {
     note.remove();
+  });
+
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
   });
 
   document.body.appendChild(note);
